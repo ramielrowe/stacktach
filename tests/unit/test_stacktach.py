1905dbd921eed46bcc6a2c9596880cdcde7e2205
Andrew Melton <andrew.melton@rackspace.com>
2013-01-28 18:00:02 -0500

    Fixing function names

--- tests/unit/test_stacktach.py
+++ tests/unit/test_stacktach.py
@@ -1,4 +1,5 @@
 import datetime
+import json
 import os
 import sys
 import unittest
@@ -168,10 +169,8 @@ class StacktachRawParsingTestCase(unittest.TestCase):
         raw = self.mox.CreateMockAnything()
         views.STACKDB.create_rawdata(**raw_values).AndReturn(raw)
         views.STACKDB.save(raw)
-        self.mox.StubOutWithMock(views, "aggregate_lifecycle")
-        views.aggregate_lifecycle(raw)
-        self.mox.StubOutWithMock(views, "aggregate_usage")
-        views.aggregate_usage(raw)
+        self.mox.StubOutWithMock(views, "aggregate")
+        views.aggregate(raw)
         self.mox.ReplayAll()
         views.process_raw_data(deployment, args, json_args)
         self.mox.VerifyAll()
@@ -197,10 +196,8 @@ class StacktachRawParsingTestCase(unittest.TestCase):
         raw = self.mox.CreateMockAnything()
         views.STACKDB.create_rawdata(**raw_values).AndReturn(raw)
         views.STACKDB.save(raw)
-        self.mox.StubOutWithMock(views, "aggregate_lifecycle")
-        views.aggregate_lifecycle(raw)
-        self.mox.StubOutWithMock(views, "aggregate_usage")
-        views.aggregate_usage(raw)
+        self.mox.StubOutWithMock(views, "aggregate")
+        views.aggregate(raw)
         self.mox.ReplayAll()
         views.process_raw_data(deployment, args, json_args)
         self.mox.VerifyAll()
@@ -233,7 +230,7 @@ class StacktachLifecycleTestCase(unittest.TestCase):
     def test_start_kpi_tracking(self):
         lifecycle = self.mox.CreateMockAnything()
         tracker = self.mox.CreateMockAnything()
-        when = utils.decimal_utcnow()
+        when = utils.decimal_utc()
         raw = utils.create_raw(self.mox, when, 'compute.instance.update',
                                host='api')
         views.STACKDB.create_request_tracker(lifecycle=lifecycle,
@@ -258,13 +255,13 @@ class StacktachLifecycleTestCase(unittest.TestCase):
 
     def test_update_kpi(self):
         lifecycle = self.mox.CreateMockAnything()
-        end = utils.decimal_utcnow()
+        end = utils.decimal_utc()
         raw = self.mox.CreateMockAnything()
         raw.request_id = REQUEST_ID_1
         raw.when=end
         timing = utils.create_timing(self.mox, 'compute.instance.create',
                                      lifecycle, end_when=end)
-        start = utils.decimal_utcnow()
+        start = utils.decimal_utc()
         tracker = utils.create_tracker(self.mox, REQUEST_ID_1, lifecycle,
                                        start)
         views.STACKDB.find_request_trackers(request_id=REQUEST_ID_1)\
@@ -279,14 +276,14 @@ class StacktachLifecycleTestCase(unittest.TestCase):
         self.assertEqual(tracker.duration, end-start)
         self.mox.VerifyAll()
 
-    def test_aggregate_lifecycle_no_instance(self):
+    def test_aggregate_no_instance(self):
         raw = self.mox.CreateMockAnything()
         raw.instance = None
         self.mox.ReplayAll()
-        views.aggregate_lifecycle(raw)
+        views.aggregate(raw)
         self.mox.VerifyAll()
 
-    def test_aggregate_lifecycle_start(self):
+    def test_aggregate_start(self):
         event_name = 'compute.instance.create'
         event = '%s.start' % event_name
         when = datetime.datetime.utcnow()
@@ -304,7 +301,7 @@ class StacktachLifecycleTestCase(unittest.TestCase):
         views.STACKDB.save(timing)
 
         self.mox.ReplayAll()
-        views.aggregate_lifecycle(raw)
+        views.aggregate(raw)
         self.assertEqual(lifecycle.last_raw, raw)
         self.assertEqual(lifecycle.last_state, 'building')
         self.assertEqual(lifecycle.last_task_state, '')
@@ -315,7 +312,7 @@ class StacktachLifecycleTestCase(unittest.TestCase):
 
         self.mox.VerifyAll()
 
-    def test_aggregate_lifecycle_end(self):
+    def test_aggregate_end(self):
         event_name = 'compute.instance.create'
         start_event = '%s.end' % event_name
         end_event = '%s.end' % event_name
@@ -341,7 +338,7 @@ class StacktachLifecycleTestCase(unittest.TestCase):
         views.STACKDB.save(timing)
 
         self.mox.ReplayAll()
-        views.aggregate_lifecycle(end_raw)
+        views.aggregate(end_raw)
         self.assertEqual(lifecycle.last_raw, end_raw)
         self.assertEqual(lifecycle.last_state, 'active')
         self.assertEqual(lifecycle.last_task_state, 'build')
@@ -356,7 +353,7 @@ class StacktachLifecycleTestCase(unittest.TestCase):
         self.mox.VerifyAll()
 
 
-    def test_aggregate_lifecycle_update(self):
+    def test_aggregate_update(self):
         event = 'compute.instance.update'
         when = datetime.datetime.utcnow()
         raw = utils.create_raw(self.mox, when, event, old_task='reboot')
@@ -371,7 +368,7 @@ class StacktachLifecycleTestCase(unittest.TestCase):
         views.start_kpi_tracking(lifecycle, raw)
 
         self.mox.ReplayAll()
-        views.aggregate_lifecycle(raw)
+        views.aggregate(raw)
         self.assertEqual(lifecycle.last_raw, raw)
         self.assertEqual(lifecycle.last_state, 'active')
         self.assertEqual(lifecycle.last_task_state, 'reboot')
