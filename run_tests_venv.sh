e6b8450f8061aba9c62d4751d5ac04550c9248bb
Andrew Melton <andrew.melton@rackspace.com>
2013-06-19 11:54:42 -0400

    Add libmysql-dev to tests_venv

--- run_tests_venv.sh
+++ run_tests_venv.sh
@@ -4,5 +4,6 @@ virtualenv --no-site-packages ".venv"
 . .venv/bin/activate
 ./.venv/bin/easy_install pip
 ./.venv/bin/pip install -r etc/test-requires.txt
+apt-get install libmysql-dev
 ./.venv/bin/pip install -r etc/pip-requires.txt
 ./.venv/bin/nosetests tests --exclude-dir=stacktach --with-coverage --cover-package=stacktach,worker,verifier --cover-erase
